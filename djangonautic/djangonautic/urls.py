1f7bd505105019ff47de33274346cb9279939110
CpBruceMeena <sandeep10198@gmail.com>
2020-01-21 11:38:39 +0530

    Displaying model content on the article page

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/$', views.about),
-    # url(r'^articles/', include('articles.urls')),
+    url(r'^articles/', include('articles.urls')),
     url(r'^$', views.homepage),
 ]
