58ea4478552be1263750ff61292eaeb78461ccf8
CpBruceMeena <sandeep10198@gmail.com>
2020-01-23 10:46:01 +0530

    Adding user to post

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -20,7 +20,9 @@ def article_create(request):
     if request.method == 'POST':
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid:
-            #save it to database
+            instance = form.save(commit = False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles:list')
     else:
         form = forms.CreateArticle()
